3097ec9813c94bb8286f55a7fb309a60c08b74bc
Abhishek Thakur <75234390+abhithakur7@users.noreply.github.com>
2024-06-04 18:05:24 +0530

    Create user.spec.tsx

---
+++ src/test/user.spec.tsx
@@ -0,0 +1,16 @@
+import User from "@/components/user";
+import { render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
+
+jest.mock("axios");
+
+test("fetches and displays user data", async () => {
+  const mockResponse = { data: { name: "John Doe" } };
+  (axios.get as jest.Mock).mockResolvedValue(mockResponse);
+
+  render(<User />);
+
+  // Check if the mocked response is used in the component
+  const userNameElement = await waitFor(() => screen.getByText(/John Doe/i));
+  expect(userNameElement).toBeInTheDocument();
+});
